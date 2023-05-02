034d7f11c1298a715fd613a59f098e812f81fd25
Cschuel <carlo.schuelting@stud.thga.de>
2023-05-02 17:40:28 +0200

    ASCII-Art Eingefügt

---
+++ PP6.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+int main() {
+	   	printf("  ________\n");
+	        printf(" /        \\\n");
+		printf("|  o    o  |\n");
+		printf("|    ^     |\n");
+		printf("|   '-'    |\n");
+		printf(" \\________/\n");
+		return 0;
+}
